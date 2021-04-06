11134d01c7d3564b72cde9821823438efca4decf
Your Name <you@example.com>
2021-04-05 21:11:01 -0500

    Finished step 8

--- assignment6/orbit-report/src/app/satellite.ts
+++ assignment6/orbit-report/src/app/satellite.ts
@@ -14,4 +14,11 @@ this.launchDate = launchDate;
 this.orbitType = orbitType;
 this.operational = operational;
 }
+shouldShowWarning(): boolean {
+    if(this.type.toLowerCase() === 'space debris') {
+        return true
+    } else {
+        return false
+    }
+  } 
 }
